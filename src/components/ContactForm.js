2ff84905d364df9e1ba4f326f922bac22cdb40da
FranklynCodes <47542708+FranklynCodes@users.noreply.github.com>
2021-11-13 14:05:52 -0500

    Fixed Form Responsive Width Issue

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,19 +6,7 @@ export default function ContactForm() {
 			<h1 className="TechStack-Title">Contact</h1>
 			<div id="FormContainer">
 				<div className="Social_Container_Contact">
-					{/* <div>
-						<a href="mailto:franklynhire@gmail.com" target={"_blank"} rel="noreferrer">
-							<i class="far fab fa-envelope"></i>
-						</a>
-					</div> */}
-
 					<div id="Social_Container">
-						{/* <div>
-							{" "}
-							<a href="/#/" target="">
-								<i className="fab fa-instagram"></i>
-							</a>
-						</div> */}
 						<div>
 							<a href="/#/" target="">
 								<i className="fab fa-linkedin"></i>
@@ -42,45 +30,27 @@ export default function ContactForm() {
 								<i className="fab fa-twitter-square"></i>
 							</a>
 						</div>
-						{/* <div>
-							{" "}
-							<a href="/#/" target="">
-								<i className="fab fa-youtube"></i>
-							</a>
-						</div> */}
-						{/* <div>
-							{" "}
-							<a href="/#/" target="">
-								<i className="fab fa-twitch"></i>
-							</a>
-						</div> */}
-						{/* <!-- <a href="/#/" target=""><i className="fab fa-facebook-square"></i></a> --> */}
 					</div>
 				</div>
 
 				<form action="" method="get" id="form_container" className="form-example">
-					<div id="address-block">
+					<div id="address-block" className="Form-Input-Container">
 						<a href="mailto:franklynhire@gmail.com" target={"_blank"} rel="noreferrer">
 							<address>franklynhire@gmail.com</address>
 						</a>
 					</div>
-					<div>
+
+					<div className="Form-Input-Container">
 						<label htmlFor="name">Name:</label>
 						<input type="text" name="name" id="name" required />
 					</div>
 
-					<div>
+					<div className="Form-Input-Container">
 						<label htmlFor="email">Email:</label>
 						<input type="email" name="email" id="email" required />
 					</div>
 
-					{/* <!-- <div className="form-example">
-						<label htmlFor="message">Message:</label>
-						<input type="text" id="message" name="message" required>
-
-					</div> --> */}
-
-					<div>
+					<div class="Form-Input-Container">
 						<label htmlFor="message">Message:</label>
 						<textarea type="text" id="message" name="message" required></textarea>
 					</div>
@@ -88,11 +58,6 @@ export default function ContactForm() {
 					<div id="button">
 						<input type="submit" value="Send" id="sendButton" />
 					</div>
-
-					{/* <!-- 					
-					<div className="form-example">
-						<button>Send</button>
-					</div> --> */}
 				</form>
 			</div>
 		</div>

136580e0ad8dbcba36ce6fba6b50e027a1e2014e
FranklynCodes <47542708+FranklynCodes@users.noreply.github.com>
2022-01-27 00:39:07 -0500

    Removal of Boiler Plate

--- src/index.js
+++ src/index.js
@@ -3,23 +3,13 @@ import ReactDOM from "react-dom";
 import "./css/main.css";
 // import "./css2/CardContainer.css";
 import cssCheck from "./checkForUndefinedCSSClasses.js";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
 import WebsiteContainer from "./container/WebsiteContainer";
 
 // cssCheck();
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<WebsiteContainer />
-		</Provider>
+		<WebsiteContainer />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
